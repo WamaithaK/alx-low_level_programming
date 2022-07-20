86aaab5e7e587fbf1f0e39612ebc2dcaeff68587
WamaithaK <wamaithakarigi@yahoo.com>
2022-07-20 07:44:54 -0700

    Task 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,6 +17,7 @@ int power_operation(int n, int c)
 	}
 	return (0 + power_operation(n, c + 1));
 }
+
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: input number.
@@ -25,10 +26,16 @@ int power_operation(int n, int c)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 	if (n == 0)
+	{
 		return (0);
+	}
 	if (n == 1)
+	{
 		return (1);
+	}
 	return (power_operation(n, 2));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int power_operation(int n, int c)
+int pow_operation(int n, int c);
+int is_prime(unsigned int n, unsigned int c);
 #endif
